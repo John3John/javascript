4a8b5553ee08478b0f249480093010810fc3fa1c
Wesley <wesleytakamine2008@gmail.com>
2025-02-25 23:38:06 -0300

    ex002.js

---
+++ ex002.js
@@ -0,0 +1,14 @@
+console.log(typeof Object)       // cria uma função 
+console.log(typeof new Object()) // cria um objeto 
+
+
+const Cliente = function() {} // cria minha própria função chamada Cliente.
+console.log(typeof Cliente)   // verificando o tipo de Cliente
+console.log(typeof new Cliente) // instanciando uma função com o 'new Cliente' que cria um objeto
+
+
+class Produto {} // ES 2015 (ES6) //Crei uma classe, mas internamente na verdade é uma função.
+console.log(typeof Produto)
+console.log(typeof new Produto()) // Instanciando essa classe temos o objeto.
+
+
