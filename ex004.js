26c0d08b0f9bc618747bfd99cd23f0655159d4ea
Wesley <wesleytakamine2008@gmail.com>
2025-02-27 14:58:54 -0300

    ex004.js

---
+++ ex004.js
@@ -0,0 +1,23 @@
+// novo recurso #2015
+
+const pessoa ={
+    nome: 'Ana',
+    idade: 5,
+    endereço: {
+        logradouro : 'Rua ABC',
+        numero:1000
+    }
+}
+
+const {nome, idade} = pessoa //{ } as chaves representam um operador de destruturação, tira de dentro pessoa o nome e a idade
+console.log(nome, idade)
+
+const {nome: n, idade: i} = pessoa
+console.log(n,i)
+
+const {sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
+
+const {endereço: {logradouro, numero, cep} } = pessoa
+console.log(logradouro,numero,cep)
+
