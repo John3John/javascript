ccce272da3ea30bc4c26040821f0c179ea579379
Wesley <wesleytakamine2008@gmail.com>
2025-03-03 14:39:16 -0300

    ex005.js

---
+++ ex005.js
@@ -0,0 +1,11 @@
+const[c] = [10]
+console.log(c)
+//destruturação simples de arrey
+
+const[n11, n12, , n13] = [1, , , 7]
+console.log(n11, n12, n13)
+//destruturação de elementos vazios no arrey
+
+const[ , [nine, , eight]] = [[ , 8, 8],[9, 6, 8]]
+console.log(nine, eight)
+//destruturação de arreys aninhados.
